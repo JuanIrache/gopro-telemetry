1c8898726e943c63791047ff50cc263264ebda8e
JuanIrache <yrache@gmail.com>
2019-05-15 04:06:25 +0200

    testing second sample file

--- index.test.js
+++ index.test.js
@@ -12,25 +12,25 @@ describe('Testing with Fusion file', () => {
     result = goproTelemetry(file);
   });
 
-  test(`${filename} should return valid metadata (DEVC)`, () => {
+  test(`Fusion should return valid metadata (DEVC)`, () => {
     expect(result.DEVC[0]).toBeDefined();
   });
-  test(`${filename} should have a data stream (STRM)`, () => {
+  test(`Fusion should have a data stream (STRM)`, () => {
     expect(result.DEVC[0].STRM.length).toBeGreaterThan(0);
   });
-  test(`${filename} should have shutter speed data (SHUT)`, () => {
+  test(`Fusion should have shutter speed data (SHUT)`, () => {
     expect(result.DEVC[0].STRM[5].SHUT.length).toBeGreaterThan(0);
   });
-  test(`${filename} should have stream names (STNM)`, () => {
+  test(`Fusion should have stream names (STNM)`, () => {
     expect(typeof result.DEVC[0].STRM[4].STNM).toBe('string');
   });
-  test(`${filename} should have scaling data (SCAL)`, () => {
+  test(`Fusion should have scaling data (SCAL)`, () => {
     expect(typeof result.DEVC[0].STRM[3].SCAL[0]).toBe('number');
   });
-  test(`${filename} should have units (SIUN)`, () => {
+  test(`Fusion should have units (SIUN)`, () => {
     expect(typeof result.DEVC[0].STRM[2].SIUN).toBe('string');
   });
-  test(`${filename} should contain ticks (TICK)`, () => {
+  test(`Fusion should contain ticks (TICK)`, () => {
     expect(typeof result.DEVC[0].STRM[1].TICK).toBe('number');
   });
 });
@@ -42,25 +42,22 @@ describe('Testing with hero5 file', () => {
     result = goproTelemetry(file);
   });
 
-  test(`${filename} should return valid metadata (DEVC)`, () => {
+  test(`hero5 should return valid metadata (DEVC)`, () => {
     expect(result.DEVC[0]).toBeDefined();
   });
-  test(`${filename} should have a data stream (STRM)`, () => {
+  test(`hero5 should have a data stream (STRM)`, () => {
     expect(result.DEVC[0].STRM.length).toBeGreaterThan(0);
   });
-  // test(`${filename} should have shutter speed data (SHUT)`, () => {
-  //   expect(result.DEVC[0].STRM[5].SHUT.length).toBeGreaterThan(0);
-  // });
-  // test(`${filename} should have stream names (STNM)`, () => {
-  //   expect(typeof result.DEVC[0].STRM[4].STNM).toBe('string');
-  // });
-  // test(`${filename} should have scaling data (SCAL)`, () => {
-  //   expect(typeof result.DEVC[0].STRM[3].SCAL[0]).toBe('number');
-  // });
-  // test(`${filename} should have units (SIUN)`, () => {
-  //   expect(typeof result.DEVC[0].STRM[2].SIUN).toBe('string');
-  // });
-  // test(`${filename} should contain ticks (TICK)`, () => {
-  //   expect(typeof result.DEVC[0].STRM[1].TICK).toBe('number');
-  // });
+  test(`hero5 should have float ISO data (ISOG)`, () => {
+    expect(result.DEVC[0].STRM[3].ISOG[0]).toBeLessThan(2);
+  });
+  test(`hero5 should have GPS time (GPSU)`, () => {
+    expect(result.DEVC[0].STRM[2].GPSU).toBe('170417173103.000');
+  });
+  test(`hero5 should have gyroscope units (SIUN)`, () => {
+    expect(result.DEVC[0].STRM[1].SIUN).toBe('rad/s');
+  });
+  test(`hero5 should have stream names (STNM)`, () => {
+    expect(result.DEVC[0].STRM[0].STNM).toBe('Accelerometer (up/down, right/left, forward/back)');
+  });
 });
