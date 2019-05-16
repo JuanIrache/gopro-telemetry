078a80acf2e7dcfeca650be532808b1de4729d58
JuanIrache <yrache@gmail.com>
2019-05-16 15:26:04 +0200

    report inner unknown types properly

--- code/parseV.js
+++ code/parseV.js
@@ -11,39 +11,39 @@ function parseV(environment, slice, len, specifics) {
   const { data, options, ks } = environment;
   const { ax = 1, type = ks.type, complexType } = specifics;
 
-  //Log unknown types for future implementation
-  if (!types[type]) unknown.add(type);
-  else {
-    //Split data when axes present
-    if (ax > 1) {
-      //Will return array of values
-      let res = [];
+  //Split data when axes present
+  if (ax > 1) {
+    //Will return array of values
+    let res = [];
 
-      for (let i = 0; i < ax; i++) {
-        let innerType = type;
-        //Pick type from previously read data if needed
-        if (types[type].complex) innerType = complexType[i];
-        res.push(parseV(environment, slice + (i * ks.size) / ax, len / ax, { ax: 1, type: innerType, complexType }));
-      }
-      return res;
+    for (let i = 0; i < ax; i++) {
+      let innerType = type;
+      //Pick type from previously read data if needed
+      if (types[type].complex) innerType = complexType[i];
+      //Log unknown types for future implementation
+      if (!types[innerType]) {
+        unknown.add(type);
+        res.push(null);
+      } else res.push(parseV(environment, slice + (i * ks.size) / ax, len / ax, { ax: 1, type: innerType, complexType }));
+    }
 
-      //Otherwise, read a single value
-    } else if (!types[type].complex) {
-      //Add options required by type
-      let opts = { length: len };
-      if (types[type].opt) for (const key in types[type].opt) opts[key] = types[type].opt[key];
-      //We pick the necessary function based on data format (stored in types)
-      let valParser = new Parser().endianess('big')[types[type].func]('value', opts);
-      const parsed = valParser.parse(data.slice(slice)).result;
+    //If debugging, print unexpected types
+    if (options.debug && unknown.size) setImmediate(() => console.log('unknown types:', [...unknown].join(',')));
+    return res;
 
-      //If debugging, print unexpected types
-      if (options.debug && unknown.size) setImmediate(() => console.log('unknown types:', [...unknown].join(',')));
+    //Otherwise, read a single value
+  } else if (!types[type].complex) {
+    //Add options required by type
+    let opts = { length: len };
+    if (types[type].opt) for (const key in types[type].opt) opts[key] = types[type].opt[key];
+    //We pick the necessary function based on data format (stored in types)
+    let valParser = new Parser().endianess('big')[types[type].func]('value', opts);
+    const parsed = valParser.parse(data.slice(slice)).result;
 
-      return parsed.value;
+    return parsed.value;
 
-      //Data is complex but did not find axes
-    } else throw new Error('Complex type ? with only one axis');
-  }
+    //Data is complex but did not find axes
+  } else throw new Error('Complex type ? with only one axis');
 }
 
 module.exports = parseV;
