3a55b11996950caf4d3901ac73186b0cc2e7cfc7
JuanIrache <yrache@gmail.com>
2019-05-16 13:44:08 +0200

    remove comments

--- samples/example.js
+++ samples/example.js
@@ -1,13 +1,13 @@
 const goproTelemetry = require(`${__dirname}/../`);
 const fs = require('fs');
 const { promisify } = require('util');
-const readFileAsync = promisify(fs.readFile); // (A)
-const writeFileAsync = promisify(fs.writeFile); // (A)
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 async function toJSON(filename) {
   try {
     const file = await readFileAsync(__dirname + filename);
-    const result = goproTelemetry(file, { debug: true });
+    const result = goproTelemetry(file, { raw: true });
     await writeFileAsync('./out.json', JSON.stringify(result));
     console.log('File saved');
   } catch (error) {
