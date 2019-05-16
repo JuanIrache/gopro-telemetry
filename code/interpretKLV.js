8fe18db1f6a442429806aad106050d1aa3a12fc1
JuanIrache <yrache@gmail.com>
2019-05-16 17:09:30 +0200

    scaling and matrix transformation probably ok

--- code/interpretKLV.js
+++ code/interpretKLV.js
@@ -2,33 +2,80 @@ function interpretKLV(klv, options) {
   let result = JSON.parse(JSON.stringify(klv));
   if (result.interpretSamples) {
     let interpreted = false;
-    if (result.hasOwnProperty('SCAL')) {
-      result[result.interpretSamples] = result[result.interpretSamples].map(s => {
-        if (typeof s === 'number') return s / result.SCAL;
-        else {
-          if (result.SCAL.length === s.length) return s.map((ss, i) => ss / result.SCAL[i]);
-          else return s.map((ss, i) => ss / result.SCAL);
+    try {
+      if (result.hasOwnProperty('SCAL')) {
+        //Loop through the samples and scale them
+        result[result.interpretSamples] = result[result.interpretSamples].map(s => {
+          //If single value, scale, otherwise loop through array
+          if (typeof s === 'number') return s / result.SCAL;
+          else {
+            //If scaling is array, apply to each "axis", otherwise apply to all
+            if (result.SCAL.length === s.length) return s.map((ss, i) => ss / result.SCAL[i]);
+            else return s.map((ss, i) => ss / result.SCAL);
+          }
+        });
+        //Done with scaling data
+        delete result.SCAL;
+        interpreted = true;
+      }
+
+      if (result.hasOwnProperty('ORIN') && result.hasOwnProperty('ORIO')) {
+        //set label for transformation Matrix
+        const labels = `(${result.ORIO.map(o => o.toLowerCase()).join(',')})`;
+        if (result.STNM) result.STNM += ` ${labels}`;
+        else result.STNM = labels;
+
+        //Transform with this if no matrix present
+        if (!result.hasOwnProperty('MTRX')) {
+          result[result.interpretSamples] = result[result.interpretSamples].map(s => {
+            let newS = [];
+            const len = result.ORIN.length;
+            for (let y = 0; y < len; y++) {
+              for (let x = 0; x < len; x++) {
+                if (result.ORIN[y].toUpperCase() === result.ORIO[x]) {
+                  if (result.ORIN[y] === result.ORIO[x]) newS[x] = s[y];
+                  else newS[x] = -s[y];
+                }
+              }
+            }
+            return newS;
+          });
         }
-      });
-      delete result.SCAL;
-      interpreted = true;
+        //Done with ORIN ORIO
+        delete result.ORIN;
+        delete result.ORIO;
+        interpreted = true;
+      }
+
+      if (result.hasOwnProperty('MTRX')) {
+        //Transform axis, including potential calibration data
+        result[result.interpretSamples] = result[result.interpretSamples].map(s => {
+          let newS = [];
+          const len = Math.sqrt(result.MTRX.length);
+          for (let y = 0; y < len; y++) {
+            for (let x = 0; x < len; x++) {
+              if (result.MTRX[y * len + x] !== 0) newS[x] = s[y] * result.MTRX[y * len + x];
+            }
+          }
+          return newS;
+        });
+        //Done with matrix
+        delete result.MTRX;
+        interpreted = true;
+      }
+
+      //Types was already used in raw, just delete the entry
+      if (result.hasOwnProperty('TYPE')) {
+        delete result.TYPE;
+        interpreted = true;
+      }
+
+      //If we did not interpret, look deeper
+      if (!interpreted) result[result.interpretSamples] = result[result.interpretSamples].map(s => interpretKLV(s, options));
+    } catch (error) {
+      setImmediate(() => console.error(error));
     }
-    if (result.hasOwnProperty('ORIN') && result.hasOwnProperty('ORIO')) {
-      //transform
-      delete result.ORIN;
-      delete result.ORIO;
-      interpreted = true;
-    }
-    if (result.hasOwnProperty('MTRX')) {
-      //TODO apply transformation here instead of ORIN, ORIO?
-      delete result.MTRX;
-      interpreted = true;
-    }
-    if (result.hasOwnProperty('TYPE')) {
-      delete result.TYPE;
-      interpreted = true;
-    }
-    if (!interpreted) result[result.interpretSamples] = result[result.interpretSamples].map(s => interpretKLV(s, options));
+    //Done with interpretation
     delete result.interpretSamples;
   }
   return result;
