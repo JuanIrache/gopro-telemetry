de7cda62d8cdd1d45c34c45f0e3fa01487dc47d0
JuanIrache <yrache@gmail.com>
2019-05-18 12:24:53 +0200

    refactor

--- code/timeKLV.js
+++ code/timeKLV.js
@@ -1,5 +1,3 @@
-let GPSUPresent, timingPresent;
-
 function toDate(d) {
   let regex = /(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})\.(\d{3})/;
   let YEAR = 1,
@@ -16,36 +14,27 @@ function toDate(d) {
 function GPSUtimes(klv) {
   let res = [];
   let initialDate;
-  if (klv.DEVC && klv.DEVC.length) {
-    klv.DEVC.forEach((d, i) => {
-      let partialRes;
-      let found;
-      if (d.STRM && d.STRM.length) {
-        d.STRM.forEach(s => {
-          if (s.GPSU != null) {
-            found = true;
-            if (!initialDate) {
-              GPSUPresent = true;
-              initialDate = toDate(s.GPSU).getTime();
-            }
-            partialRes = { date: toDate(s.GPSU) };
-            if (res.length) {
-              res[res.length - 1].duration = partialRes.date - res[res.length - 1].date;
-              if (i + 1 === klv.DEVC.length) partialRes.duration = res[res.length - 1].duration;
-            }
-          }
-        });
-      }
-      if (GPSUPresent) {
-        if (!found) {
-          partialRes.duration = res[i - 1].duration;
-          partialRes.date = res[i - 1].date + res[i - 1].duration;
-        }
-        partialRes.cts = partialRes.date.getTime() - initialDate;
-        res.push(partialRes);
-      }
-    });
-  }
+  klv.DEVC.forEach((d, i) => {
+    let partialRes;
+    let date;
+    if (d.STRM)
+      d.STRM.forEach(s => {
+        if (s.GPSU != null) date = toDate(s.GPSU);
+      });
+    if (date) {
+      if (!initialDate) initialDate = date.getTime();
+      partialRes = { date };
+      // Assign duration for previous pack. The last one will lack it
+      if (res.length) res[res.length - 1].duration = partialRes.date - res[res.length - 1].date;
+    } else if (res.length > 1) {
+      res[i - 1].duration = res[i - 2].duration;
+      partialRes.date = res[i - 1].date + res[i - 1].duration;
+    }
+    if (partialRes) {
+      partialRes.cts = partialRes.date.getTime() - initialDate;
+      res.push(partialRes);
+    }
+  });
 
   return res;
 }
@@ -54,53 +43,47 @@ function providedTimes(klv, timing) {
   let res = [];
   if (timing.samples.length) {
     const initialDate = timing.start.getTime();
-    if (klv.DEVC && klv.DEVC.length) {
-      klv.DEVC.forEach((d, i) => {
-        let partialRes;
-        if (timing.samples[i] != null) {
-          if (!timingPresent) timingPresent = true;
-          partialRes = JSON.parse(JSON.stringify(timing.samples[i]));
-        } else {
-          partialRes.duration = res[i - 1].duration;
-          partialRes.cts = res[i - 1].cts + res[i - 1].duration;
-        }
-        partialRes.date = new Date(initialDate + partialRes.cts);
-        res.push(partialRes);
-      });
-    }
+    klv.DEVC.forEach((d, i) => {
+      let partialRes;
+      if (timing.samples[i] != null) partialRes = JSON.parse(JSON.stringify(timing.samples[i]));
+      else {
+        partialRes.cts = res[i - 1].cts + res[i - 1].duration;
+        //Don't assume previous duration if last pack of samples. Could be shorter
+        if (i + 1 < klv.DEVC.length) partialRes.duration = res[i - 1].duration;
+      }
+      partialRes.date = new Date(initialDate + partialRes.cts);
+      res.push(partialRes);
+    });
   }
 
   return res;
 }
 
 function timeKLV(klv, timing, options) {
-  const gpsTimes = GPSUtimes(klv);
-  const pTimes = providedTimes(klv, timing);
   let result = JSON.parse(JSON.stringify(klv));
   if (result.DEVC && result.DEVC.length) {
+    const gpsTimes = GPSUtimes(klv);
+    const pTimes = providedTimes(klv, timing);
     result.DEVC.forEach((d, i) => {
       let cts, duration;
-      let skip;
-      if (timingPresent) {
+      if (pTimes.length) {
         cts = pTimes[i].cts;
         duration = pTimes[i].duration;
-      } else if (GPSUPresent) {
+      } else if (gpsTimes.length) {
         cts = gpsTimes[i].cts;
         duration = gpsTimes[i].duration;
-      } else skip = true;
+      }
       if (d.STRM && d.STRM.length) {
         d.STRM.forEach(s => {
           if (s.interpretSamples && s[s.interpretSamples].length) {
             const sDur = duration / s[s.interpretSamples].length; //see if TSMP and //EMPT are useful here
             let sCts = cts;
             s[s.interpretSamples] = s[s.interpretSamples].map(ss => {
-              if (!skip) {
+              if (cts != null) {
                 const time = sCts;
                 sCts += sDur;
                 return { time, value: ss };
-              } else {
-                return { value: ss };
-              }
+              } else return { value: ss };
             });
           }
         });
