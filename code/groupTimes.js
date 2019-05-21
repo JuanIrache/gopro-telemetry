9b38e748595107ed97794579952068f49f5f285e
JuanIrache <yrache@gmail.com>
2019-05-21 09:27:14 +0200

    grouptimes mostly working for long times

--- code/groupTimes.js
+++ code/groupTimes.js
@@ -1,20 +1,89 @@
-function mergeSamples(samples) {}
+//Receives options from main function
+let timeOut;
 
-module.exports = function(samples, length) {
-  let currentTime = 0;
-  let result = [];
-  let reachedEnd = false;
-  let i = 0;
-  while (!reachedEnd) {
-    let group = [];
-    while (samples[i].cts < currentTime + length) {
-      group.push(samples[i]);
-      if (i + 1 === samples.length) {
-        reachedEnd = true;
-        break;
-      } else i++;
+//From many samples, build one
+function mergeSamples(samples) {
+  //Get all unique keys
+  const keys = new Set(samples.reduce((acc, curr) => acc.concat(Object.keys(curr)), []));
+  let result = Array.isArray(samples[0]) ? [] : {};
+  //Loop the keys
+  keys.forEach(k => {
+    //With valid values
+    const validVals = samples.map(s => s[k]).filter(v => v != null);
+    //If number, calculate average dividing valid values by total
+    if (!isNaN(validVals[0])) result[k] = validVals.reduce((acc, curr, i, arr) => acc + curr / arr.length, 0);
+    //If object (or more likely array) merge the samples recursively
+    else if (typeof validVals[0] === 'object') result[k] = mergeSamples(validVals);
+    //Preserve null values
+    else if (validVals[0] === undefined) result[k] = null;
+    //If string or other, use the first valid value
+    else result[k] = validVals[0];
+  });
+  //If cts was temporary, remove it
+  if (timeOut === 'date') delete result.cts;
+  return result;
+}
+
+//Build one sample by interpolating the previous and the next
+function interpolateSample(samples, i, currentTime) {
+  const baseTime = samples[i].cts;
+  const difference = samples[i + 1].cts - baseTime;
+  //How much we need to move from i towards i+1
+  const proportion = (currentTime - baseTime) / difference;
+  //Get all unique keys
+  const keys = new Set([samples[i], samples[i + 1]].reduce((acc, curr) => acc.concat(Object.keys(curr)), []));
+  let result = {};
+  //Loop the keys
+  keys.forEach(k => {
+    const validVals = twoSamples.map(s => s[k]).filter(v => v != null);
+    if (typeof validVals[0] === 'number') {
+      //If number, calculate proportion
+      //With two valid values, otherwise assign the valid one
+      if (validVals.length > 1) result[k] = validVals[0] + (validVals[1] - validVals[0]) * proportion;
+      else result[k] = validVals[0];
+    } else if (typeof validVals[0] === 'object') {
+      //If object (or more likely array) interpolate the samples recursively
+      result[k] = interpolateSample(validVals, i, currentTime);
+    } else {
+      //If string or other, use the first valid value
+      result[k] = validVals[0];
     }
-    result.push(mergeSamples(group));
-    currentTime += length;
+  });
+  //If cts was temporary, remove it
+  if (timeOut === 'date') delete result.cts;
+  return result;
+}
+
+module.exports = function(klv, { groupTimes, timeOut }) {
+  timeOut = timeOut;
+  let result = JSON.parse(JSON.stringify(klv));
+  for (const key in result) {
+    if (result[key].streams) {
+      for (const k in result[key].streams) {
+        const samples = result[key].streams[k].samples;
+        if (samples) {
+          let currentTime = 0;
+          let newSamples = [];
+          let reachedEnd = false;
+          let i = 0;
+          while (!reachedEnd) {
+            let group = [];
+            while (samples[i].cts < currentTime + groupTimes) {
+              group.push(samples[i]);
+              if (i + 1 === samples.length) {
+                reachedEnd = true;
+                break;
+              } else i++;
+            }
+            if (samples.length > 1) newSamples.push(mergeSamples(group));
+            else if (samples.lenght === 1) newSamples.push(group[0]);
+            else if (i < samples.length) newSamples.push(interpolateSample(samples, i - 1, currentTime));
+            currentTime += groupTimes;
+          }
+          result[key].streams[k].samples = newSamples;
+        }
+      }
+    }
   }
+  return result;
 };
--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const streamList = require('./code/streamList');
 const timeKLV = require('./code/timeKLV');
 const interpretKLV = require('./code/interpretKLV');
 const mergeStream = require('./code/mergeStream');
+const groupTimes = require('./code/groupTimes');
 
 module.exports = function(input, options = {}) {
   //Create filter arrays if user didn't
   if (options.device && !Array.isArray(options.device)) options.device = [options.device];
   if (options.stream && !Array.isArray(options.stream)) options.stream = [options.stream];
-  //Read framerate to convert groupTimes to number if needed
-  if (options.groupTimes === 'frames') options.groupTimes = input.timing.frameDuration;
   //Parse input
   const parsed = parseKLV(input.rawData, options);
   if (!parsed.DEVC) {
@@ -35,5 +34,9 @@ module.exports = function(input, options = {}) {
   let merged = {};
   //Merge samples in sensor entries
   for (const key in timed) merged[key] = mergeStream(timed[key], options);
+  //Read framerate to convert groupTimes to number if needed
+  if (options.groupTimes === 'frames') options.groupTimes = input.timing.frameDuration;
+  //Group samples by time if necessary
+  if (options.groupTimes) merged = groupTimes(merged, options);
   return merged;
 };
