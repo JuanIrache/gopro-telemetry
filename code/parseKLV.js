761280aa4ace4b2ad59794cccf8b5cc9694f2bad
JuanIrache <yrache@gmail.com>
2019-06-06 20:21:25 +0200

    drastically improve performance when deviceList

--- code/parseKLV.js
+++ code/parseKLV.js
@@ -18,67 +18,72 @@ function parseKLV(data, options = {}, start = 0, end = data.length, parent) {
       //Parse the first 2 sections (64 bits) of each KLV to decide what to do with the third
       const ks = keyAndStructParser.parse(data.slice(start)).result;
 
-      //Get the length of the value (or values, or nested values)
-      length = ks.size * ks.repeat;
+      //Abort if we are creating a device list. We have enough info
+      if (ks.fourCC === 'STRM' && options.deviceList) {
+        lastCC = ks.fourCC;
+      } else {
+        //Get the length of the value (or values, or nested values)
+        length = ks.size * ks.repeat;
 
-      //Advance to the next KLV, at least 64 bits
-      reached = start + 8 + (length >= 0 ? length : 0);
-      tempStart = start;
-      //Align to 32 bits
-      while (tempStart < reached) tempStart += 4;
-      //Find if this is the last CC of the nest and emember it to keep it as array
-      if (tempStart >= end) lastCC = ks.fourCC;
+        //Advance to the next KLV, at least 64 bits
+        reached = start + 8 + (length >= 0 ? length : 0);
+        tempStart = start;
+        //Align to 32 bits
+        while (tempStart < reached) tempStart += 4;
+        //Find if this is the last CC of the nest and emember it to keep it as array
+        if (tempStart >= end) lastCC = ks.fourCC;
 
-      let partialResult = [];
-      //Check if the lastCC is to be filtered out by options
-      if (lastCC && parent === 'STRM' && options.stream && !options.stream.includes(ks.fourCC)) return null;
-      else if (length >= 0) {
-        //Remember last key for interpreting data later
-        if (lastCC) result.interpretSamples = ks.fourCC;
+        let partialResult = [];
+        //Check if the lastCC is to be filtered out by options
+        if (lastCC && parent === 'STRM' && options.stream && !options.stream.includes(ks.fourCC)) return null;
+        else if (length >= 0) {
+          //Remember last key for interpreting data later
+          if (lastCC) result.interpretSamples = ks.fourCC;
 
-        //If empty, we still want to store the fourCC
-        if (length === 0) partialResult.push(null);
-        //Log unknown types for future implementation
-        else if (!types[ks.type]) unknown.add(ks.type);
-        //Recursive call to parse nested data
-        else if (types[ks.type].nested) {
-          const parsed = parseKLV(data, options, start + 8, start + 8 + length, ks.fourCC);
-          if (parsed != null) partialResult.push(parsed);
-        }
-        //We can parse the Value
-        else if (types[ks.type].func || (types[ks.type].complex && complexType)) {
-          //Detect data with multiple axes
-          let axes = 1;
-          if (types[ks.type].size > 1) axes = ks.size / types[ks.type].size;
-          //Detect them when the type is complex
-          else if (types[ks.type].complex && complexType.length) axes = complexType.length;
-          //Human readable strings should de merged for readability
-          if (mergeStrings.includes(ks.fourCC)) {
-            ks.size = length;
-            ks.repeat = 1;
+          //If empty, we still want to store the fourCC
+          if (length === 0) partialResult.push(null);
+          //Log unknown types for future implementation
+          else if (!types[ks.type]) unknown.add(ks.type);
+          //Recursive call to parse nested data
+          else if (types[ks.type].nested) {
+            const parsed = parseKLV(data, options, start + 8, start + 8 + length, ks.fourCC);
+            if (parsed != null) partialResult.push(parsed);
           }
+          //We can parse the Value
+          else if (types[ks.type].func || (types[ks.type].complex && complexType)) {
+            //Detect data with multiple axes
+            let axes = 1;
+            if (types[ks.type].size > 1) axes = ks.size / types[ks.type].size;
+            //Detect them when the type is complex
+            else if (types[ks.type].complex && complexType.length) axes = complexType.length;
+            //Human readable strings should de merged for readability
+            if (mergeStrings.includes(ks.fourCC)) {
+              ks.size = length;
+              ks.repeat = 1;
+            }
 
-          const environment = { data, options, ks };
-          const specifics = { ax: axes, complexType };
+            const environment = { data, options, ks };
+            const specifics = { ax: axes, complexType };
 
-          //Access the values or single value
-          if (ks.repeat > 1) {
-            for (let i = 0; i < ks.repeat; i++) partialResult.push(parseV(environment, start + 8 + i * ks.size, ks.size, specifics));
-          } else partialResult.push(parseV(environment, start + 8, length, specifics));
-          //If we just read a TYPE value, store it. Will be necessary in this nest
-          if (ks.fourCC === 'TYPE') complexType = partialResult[0];
-          //Abort out if we are selecting devices and this one is not selected
-          else if (ks.fourCC === 'DVID' && parent === 'DEVC' && options.device && !options.device.includes(partialResult[0])) return null;
+            //Access the values or single value
+            if (ks.repeat > 1) {
+              for (let i = 0; i < ks.repeat; i++) partialResult.push(parseV(environment, start + 8 + i * ks.size, ks.size, specifics));
+            } else partialResult.push(parseV(environment, start + 8, length, specifics));
+            //If we just read a TYPE value, store it. Will be necessary in this nest
+            if (ks.fourCC === 'TYPE') complexType = partialResult[0];
+            //Abort out if we are selecting devices and this one is not selected
+            else if (ks.fourCC === 'DVID' && parent === 'DEVC' && options.device && !options.device.includes(partialResult[0])) return null;
 
-          //Something went wrong, store type for debugging
-        } else unknown.add(ks.type);
+            //Something went wrong, store type for debugging
+          } else unknown.add(ks.type);
 
-        if (result.hasOwnProperty(ks.fourCC)) {
-          result[ks.fourCC] = result[ks.fourCC].concat(partialResult);
-        } else result[ks.fourCC] = partialResult;
+          if (result.hasOwnProperty(ks.fourCC)) {
+            result[ks.fourCC] = result[ks.fourCC].concat(partialResult);
+          } else result[ks.fourCC] = partialResult;
 
-        //Parsing error
-      } else throw new Error('Error, negative length');
+          //Parsing error
+        } else throw new Error('Error, negative length');
+      }
     } catch (err) {
       setImmediate(() => console.error(err));
     }
