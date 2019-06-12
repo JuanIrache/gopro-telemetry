243d1990ad86d145d935645735e7f18977e2f68e
JuanIrache <yrache@gmail.com>
2019-06-12 13:40:57 +0200

    unit test tokml

---
+++ __tests__/toKml.test.js
@@ -0,0 +1,21 @@
+const toKml = require('../code/toKml');
+const { readFileSync } = require('fs');
+
+const file = readFileSync(`${__dirname}/../samples/partials/mergedGps.json`);
+const result = toKml(JSON.parse(file), {});
+
+test(`toKml should return a long string`, () => {
+  expect(result.length).toBeGreaterThan(7500);
+});
+
+test(`toKml should start with xml-kml format`, () => {
+  expect(result.slice(0, 196)).toBe(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.0">
+    <Document>
+        <name>undefined</name>
+        <description>Camera. GPS (Lat., Long., Alt., 2D speed, 3D`
+  );
+});
+
+console.log(result.slice(0, 197));
