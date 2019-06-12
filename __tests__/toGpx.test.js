3473c61c2045a82b1a106d6c2e3609f94746ba41
JuanIrache <yrache@gmail.com>
2019-06-12 13:33:53 +0200

    typo

--- __tests__/toGpx.test.js
+++ __tests__/toGpx.test.js
@@ -8,7 +8,7 @@ test(`toGpx should return a long string`, () => {
   expect(result.length).toBeGreaterThan(6000);
 });
 
-test(`toGpx should start with html-gps format`, () => {
+test(`toGpx should start with xml-gpx format`, () => {
   expect(result.slice(0, 197)).toBe(
     `<?xml version="1.0" encoding="UTF-8"?>
 <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="https://github.com/juanirache/gopro-telemetry">
