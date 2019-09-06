771046d6b6d2c2c86eda47380e510ac9550224ca
JuanIrache <yrache@gmail.com>
2019-09-06 19:00:58 +0200

    reintroduce loose overlapping constraints for safety

--- code/setSourceOffset.js
+++ code/setSourceOffset.js
@@ -1,6 +1,9 @@
 //Check that out time is before next in time, then apply offsets
 module.exports = (a, b) => {
+  const lastSample = a.samples[a.samples.length - 1];
+  const lastTime = a.start.getTime() + lastSample.cts;
   const nextStart = b.start.getTime();
+  if (nextStart - lastTime < 0) throw new Error('Sources overlap in time. Cannot merge');
   const firstCts = a.samples[0].cts;
   const firstStart = a.start.getTime();
   b.offset = firstCts + nextStart - firstStart;
