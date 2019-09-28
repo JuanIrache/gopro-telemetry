e402a4459f25362c12d669ddcbb9c54c69963acb
JuanIrache <yrache@gmail.com>
2019-09-28 16:15:20 +0200

    keep accepting merges from overlapping sources, but force offset them to stop overlapping

--- code/setSourceOffset.js
+++ code/setSourceOffset.js
@@ -4,5 +4,13 @@ module.exports = (a, b) => {
   const firstCts = a.samples[0].cts;
   const firstStart = a.start.getTime();
   b.offset = firstCts + nextStart - firstStart;
+  const aLastSampleIndex = a.samples.length - 1;
+  //Find where the first video ends
+  const firstEnd =
+    a.samples[aLastSampleIndex].cts +
+    (a.samples[aLastSampleIndex].duration || a.samples[aLastSampleIndex - 1].duration);
+  //Make sure videos are not overlapping, might involve a loss of precision.
+  b.offset = Math.max(firstEnd, b.offset);
+
   b.samples = b.samples.map(s => ({ ...s, cts: s.cts + b.offset }));
 };
