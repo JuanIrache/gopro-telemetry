230928e69a5b4ece257c44495e316185de9a1dfd
JuanIrache <yrache@gmail.com>
2019-10-25 00:37:57 +0200

    split gpx into gpx and virb for better restuls on virb edit

--- __tests__/toGpx.test.js
+++ __tests__/toGpx.test.js
@@ -11,10 +11,10 @@ test(`toGpx should return a long string`, () => {
 test(`toGpx should start with xml-gpx format`, () => {
   expect(
     result
-      .slice(370, 950)
+      .slice(300, 960)
       .replace(/\s/g, '')
       .trim()
   ).toBe(
-    `<src>Camera</src><trkseg><trkptlat=\"33.1264969\"lon=\"-117.3273542\"><ele>-20.184</ele><time>2017-04-17T17:31:03.000Z</time><fix>3</fix><hdop>606</hdop><geoidheight>-34.03217630752571</geoidheight><extensions><gpxtpx:TrackPointExtension><gpxtpx:speed>0.19</gpxtpx:speed></gpxtpx:TrackPointExtension></extensions><cmt>2dSpeed:0.167;3dSpeed:0.19</cmt>`
+    `<src>Camera</src><trkseg><trkptlat=\"33.1264969\"lon=\"-117.3273542\"><ele>-20.184</ele><time>2017-04-17T17:31:03.000Z</time><fix>3</fix><hdop>606</hdop><geoidheight>-34.03217630752571</geoidheight><cmt>2dSpeed:0.167;3dSpeed:0.19</cmt></trkpt><trkptlat=\"33.1264969\"lon=\"-117.3273541\"><ele>-20.146</ele><time>2017-04-17T17:31:03.055Z</time><fix>3</fix><hdop>606</hdop><geoidheight>-34.03217630752571</geoidheight>`
   );
 });
---
+++ __tests__/toVirb.test.js
@@ -0,0 +1,20 @@
+const toVirb = require('../code/toVirb');
+const { readFileSync } = require('fs');
+
+const file = readFileSync(`${__dirname}/../samples/partials/mergedGps.json`);
+const result = toVirb(JSON.parse(file), {});
+
+test(`toVirb should return a long string`, () => {
+  expect(result.length).toBeGreaterThan(6000);
+});
+
+test(`toVirb should start with xml- (virb) format`, () => {
+  expect(
+    result
+      .slice(370, 940)
+      .replace(/\s/g, '')
+      .trim()
+  ).toBe(
+    `<src>Camera</src><trkseg><trkptlat=\"33.1264969\"lon=\"-117.3273542\"><ele>-20.184</ele><time>2017-04-17T17:31:03.000Z</time><geoidheight>undefined</geoidheight><extensions><gpxtpx:TrackPointExtension><gpxtpx:speed>0.19</gpxtpx:speed></gpxtpx:TrackPointExtension></extensions></trkpt><trkptlat=\"33.1264969\"lon=\"-117.3273541\"><ele>-20.146</ele>`
+  );
+});
