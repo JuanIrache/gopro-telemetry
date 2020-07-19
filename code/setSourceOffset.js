ad75c43891861d4ac58cee6c4d95db18323645a6
JuanIrache <yrache@gmail.com>
2020-07-19 18:56:51 +0200

    add breathing to setSourceOffset

--- code/setSourceOffset.js
+++ code/setSourceOffset.js
@@ -1,5 +1,7 @@
+const breathe = require('./utils/breathe');
+
 //Check that out time is before next in time, then apply offsets
-module.exports = (a, b) => {
+module.exports = async (a, b) => {
   const nextStart = b.start.getTime();
   const firstCts = a.samples[0].cts;
   const firstStart = a.start.getTime();
@@ -8,9 +10,16 @@ module.exports = (a, b) => {
   //Find where the first video ends
   const firstEnd =
     a.samples[aLastSampleIndex].cts +
-    (a.samples[aLastSampleIndex].duration || a.samples[aLastSampleIndex - 1].duration);
+    (a.samples[aLastSampleIndex].duration ||
+      a.samples[aLastSampleIndex - 1].duration);
   //Make sure videos are not overlapping, might involve a loss of precision.
   b.offset = Math.max(firstEnd, b.offset);
 
-  b.samples = b.samples.map(s => ({ ...s, cts: s.cts + b.offset }));
+  const newBSamples = [];
+  for (let i = 0; i < b.samples.length; i++) {
+    if (i % 20000 === 0) await breathe();
+    const s = b.samples[i];
+    newBSamples.push({ ...s, cts: s.cts + b.offset });
+  }
+  b.samples = newBSamples;
 };
