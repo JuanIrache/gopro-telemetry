6f784c6345f81b852b773ee9378b438f434b10b6
JuanIrache <yrache@gmail.com>
2020-07-24 17:36:10 +0200

    update test for simplified virb format

--- __tests__/toVirb.test.js
+++ __tests__/toVirb.test.js
@@ -13,12 +13,12 @@ describe('Test GPS5', () => {
   });
 
   test(`toVirb should return a long string`, () => {
-    expect(result.length).toBeGreaterThan(6000);
+    expect(result.length).toBeGreaterThan(4000);
   });
 
   test(`toVirb should start with xml- (virb) format`, () => {
-    expect(result.slice(453, 1023).replace(/\s/g, '').trim()).toBe(
-      `<src>Camera</src><trkseg><trkptlat=\"33.1264969\"lon=\"-117.3273542\"><ele>-20.184</ele><time>2017-04-17T17:31:03.000Z</time><geoidheight>-34.03217630752571</geoidheight><extensions><gpxtpx:TrackPointExtension><gpxtpx:speed>0.19</gpxtpx:speed></gpxtpx:TrackPointExtension></extensions></trkpt><trkptlat=\"33.1264969\"lon=\"-117.3273541\"><ele>-20.146</ele>`
+    expect(result.slice(383, 736).replace(/\s/g, '').trim()).toBe(
+      `<src>Camera</src><trkseg><trkptlat=\"33.1264969\"lon=\"-117.3273542\"><ele>-20.184</ele><time>2017-04-17T17:31:03Z</time><geoidheight>-34.03217630752571</geoidheight></trkpt><trkptlat=\"33.1264969\"lon=\"-117.3273541\"><ele>-20.146</ele>`
     );
   });
 });
