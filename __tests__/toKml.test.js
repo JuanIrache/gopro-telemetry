fbf627c64d687a6bfe2b1cd92ee6b7011a1bc12f
JuanIrache <yrache@gmail.com>
2020-10-15 17:03:12 +0200

    fix toKml test

--- __tests__/toKml.test.js
+++ __tests__/toKml.test.js
@@ -16,13 +16,16 @@ describe('Test KML', () => {
     expect(result.length).toBeGreaterThan(7500);
   });
 
-  test(`toKml should start with xml-kml format`, () => {
-    expect(result.slice(0, 196)).toBe(
-      `<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2">
-    <Document>
-        <name>undefined</name>
-        <description>Camera. 30 fps. GPS (Lat., Long., Alt., 2D sp`
-    );
+  test(`toKml should export kml placemarks`, () => {
+    expect(result.slice(478, 918)).toBe(`<Placemark>
+            <description>GPS Fix: 3; GPS Accuracy: 606; geoidHeight: -34.03217630752571; 2D Speed: 0.167; 3D Speed: 0.19</description>
+            <Point>
+                <altitudeMode>absolute</altitudeMode>
+                <coordinates>-117.3273542,33.1264969,-20.184</coordinates>
+            </Point>
+            <TimeStamp>
+                <when>2017-04-17T17:31:03.000Z</when>
+            </TimeStamp>
+        </Placemark>`);
   });
 });
