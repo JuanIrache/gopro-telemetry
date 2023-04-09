6a4a34b1214b748c56ba2e9c5c37f8a65dcc532a
Juan Irache <yrache@gmail.com>
2023-04-09 20:10:48 +0200

    avoid bad calculations with date

--- code/utils/reduceSamples.js
+++ code/utils/reduceSamples.js
@@ -9,16 +9,16 @@ function reduceSamples(samples) {
   keys.forEach(k => {
     //With valid values
     const validVals = samples.map(s => s[k]).filter(v => v != null);
-    //If number, calculate average dividing valid values by total
-    if (!isNaN(validVals[0])) {
-      result[k] =
-        validVals.reduce((acc, curr) => acc + curr, 0) / validVals.length;
-      //If date, calculate average dividing all by total
-    } else if (k === 'date') {
+    //If date, calculate average dividing all by total
+    if (k === 'date') {
       result[k] = new Date(
         validVals.reduce((acc, curr) => acc + new Date(curr).getTime(), 0) /
           validVals.length
       );
+      //If number, calculate average dividing valid values by total
+    } else if (!isNaN(validVals[0])) {
+      result[k] =
+        validVals.reduce((acc, curr) => acc + curr, 0) / validVals.length;
       //If object (or more likely array) merge the samples recursively
     } else if (typeof validVals[0] === 'object') {
       result[k] = reduceSamples(validVals);
