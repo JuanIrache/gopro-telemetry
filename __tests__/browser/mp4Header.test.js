2dcfa57ed3772fb6fe9fdb5ad9aa9cc6453b23c8
Juan Irache <yrache@gmail.com>
2023-04-22 10:28:39 +0200

    more test time increases

--- __tests__/browser/mp4Header.test.js
+++ __tests__/browser/mp4Header.test.js
@@ -6,14 +6,20 @@ describe('Testing with mp4 header', () => {
   let inputHandle;
   let result;
   beforeAll(async () => {
-    await page.goto(`file://${join(__dirname, './index.html')}`, { waitUntil: 'networkidle0' });
+    await page.goto(`file://${join(__dirname, './index.html')}`, {
+      waitUntil: 'networkidle0'
+    });
     inputHandle = await page.$('input[type=file]');
-    await inputHandle.uploadFile(join(__dirname, '../../samples/mp4header.raw'));
-    result = await page.evaluate(async () => GoProTelemetry(
-      { rawData: await readFileAsBuffer(0) },
-      { mp4header: true },
-    ));
-  });
+    await inputHandle.uploadFile(
+      join(__dirname, '../../samples/mp4header.raw')
+    );
+    result = await page.evaluate(async () =>
+      GoProTelemetry(
+        { rawData: await readFileAsBuffer(0) },
+        { mp4header: true }
+      )
+    );
+  }, 10000);
 
   test(`The sample should have 3 highlight tags`, () => {
     expect(result.HLMT.streams.HLMT.samples.length).toBe(3);
